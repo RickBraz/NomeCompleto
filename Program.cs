470904b2b7a4419d03722bbdacd9c975deeba916
= <=>
2020-05-15 01:12:17 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("bom dia usuario por favor digite seu nome: ");
+
+            string nome = Console.ReadLine();
+
+            Console.Write("agora digite seu sobrenome: ");
+
+            string sobrenome = Console.ReadLine();
+            string NomeCompleto = nome + " " + sobrenome;
+            string NomeCatálogo = sobrenome .ToUpper() + ", " + nome;
+
+            Console.WriteLine(NomeCompleto);
+            Console.WriteLine(NomeCatálogo);
+        }
+    }
+}
